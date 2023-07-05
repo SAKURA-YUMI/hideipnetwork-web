f844eaa7aabf267c5da7c934c1979e3f78506585
SAKURA <91626087+SAKURA-YUMI@users.noreply.github.com>
2023-07-05 14:22:59 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: `/server/`,
-    bare: `/https://accip.cloudflared.cf/`,
+    bare: `/https://hideip.cloudflared.cf/`,
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/server/server.handler.js',
